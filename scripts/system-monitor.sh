c116005f3efa3e2a4a1aa1cbf5db615625588b03
Swati1019 <swatichoudhary1910@gmail.com>
2026-04-03 18:45:17 +0000

    updated code with colours

--- scripts/system-monitor.sh
+++ scripts/system-monitor.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo "===== SYSTEM MONITOR ====="
+echo -e "\e[1;36m===== SYSTEM MONITOR =====\e[0m"
 echo ""
 
 echo "--- CPU Usage ---"
-top -bn1 | awk '/^Cpu(s)/{print "CPU Usage: " $2 "%"}'
+top -bn1 | awk '/%Cpu/{print "\033[32mCPU Usage: " $2 "%\033[0m"}'
 
 echo ""
 echo "--- Memory Usage ---"
-free -h | awk '/^Mem/ {print "Total: " $2 "  Used: " $3 "  Free: " $4}'
+free -h | awk '/^Mem/ {print "\033[32mTotal: " $2 "\033[0m \033[31mUsed: " $3 "\033[0m \033[32mFree: " $4"\033[0m"}'
 
 echo ""
 echo "--- Disk Space ---"
