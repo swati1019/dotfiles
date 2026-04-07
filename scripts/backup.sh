06a268d823c39f315e83ca380c93fa7ed7a784ee
Swati1019 <swatichoudhary1910@gmail.com>
2026-04-07 12:46:17 +0530

    Add backup.sh script

---
+++ scripts/backup.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+SOURCE="/home/swati"
+BACKUP_DIR="/var/backup"
+LOG="/var/backup.log"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+mkdir -p $BACKUP_DIR
+
+echo "[$TIMESTAMP] BACKUP STARTED" >> $LOG
+rsync -av --exclude='.git' $SOURCE $BACKUP_DIR >> $LOG 2>&1
+
+echo "[$TIMESTAMP] BACKUP FINISHED" >> $LOG
+echo "DONE CHECK $LOG FOR DETAILS"
